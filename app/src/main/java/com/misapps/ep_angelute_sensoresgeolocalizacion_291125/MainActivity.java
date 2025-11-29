ef8d98d984952675eb890c9988f3bb6d60e72f78
Ángel96102 <aute2024@incos-santacruz.edu.bo>
2025-11-29 16:28:40 -0400

    mainPrincipa

--- app/src/main/java/com/misapps/ep_angelute_sensoresgeolocalizacion_291125/MainActivity.java
+++ app/src/main/java/com/misapps/ep_angelute_sensoresgeolocalizacion_291125/MainActivity.java
@@ -1,20 +1,45 @@
-package com.misapps.ep_angelute_sensoresgeolocalizacion_291125;
-
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.misapps.ep_angelute_sensoresgeolocalizacion_291125.R;
+
+public class MainActivity extends AppCompatActivity implements OnSensorDataListener {
+
+    private MapaFragment mapaFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState == null) {
+
+            SensorFragment sensorFragment = new SensorFragment();
+            mapaFragment = new MapaFragment();
+
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.sensor_fragment_container, sensorFragment)
+                    .replace(R.id.map_fragment_container, mapaFragment, "mapa_fragment_tag")
+                    .commit();
+        } else {
+
+            mapaFragment = (MapaFragment) getSupportFragmentManager().findFragmentByTag("mapa_fragment_tag");
+        }
+    }
+
+
+
+    @Override
+    public void onSensorDataChanged(float[] values) {
+
+    }
+
+    @Override
+    public void onDeviceMoved() {
+
+        if (mapaFragment != null) {
+            mapaFragment.onDeviceMoved();
+        }
     }
 }
